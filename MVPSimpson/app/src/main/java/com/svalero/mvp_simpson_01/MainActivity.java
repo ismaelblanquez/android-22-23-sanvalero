cf150a4a531b61805f46b51062135cf4cf152618
ismaelblanquez <ismaelblanquez@gmail.com>
2023-02-07 09:38:25 +0100

    07/02/23
    Simpson MVP

---
+++ MVPSimpson/app/src/main/java/com/svalero/mvp_simpson_01/MainActivity.java
@@ -0,0 +1,30 @@
+package com.svalero.mvp_simpson_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.svalero.mvp_simpson_01.lst_simpson.view.LstSimpsonActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        // SetTimeOut - setInterval
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent screenChanger = new Intent(getBaseContext(),
+                        LstSimpsonActivity.class
+                );
+                startActivity(screenChanger);
+            }
+        }, 4000);
+    }
+}
